5da29e46d858fa8cd7adadd23f4d3bea8779df60
Glory Kim <glorydkim@gmail.com>
2017-10-27 12:06:12 -0400

    Update Solution.java

--- Day 3 - Intro to Conditional Statements/Solution.java
+++ Day 3 - Intro to Conditional Statements/Solution.java
@@ -51,21 +51,19 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 public class Solution {
-   
-   public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int n = scan.nextInt(); 
-      scan.close();
-      String ans="";
-          
-      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
-      if(n%2==1){
-         ans = "Weird";
-      }
-      else{
-         // Complete the code 
 
-      }
-      System.out.println(ans);
-   }
-}
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+            if(n%2==1 || ( (n%2==0) && (n>=6 && n <= 20 ) )){
+              ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+            System.out.println(ans);
+            
+        }
+    }
+   
