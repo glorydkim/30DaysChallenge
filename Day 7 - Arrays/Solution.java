d5ac48061ea3ae1e41300d05e88a0fbd00d1fecd
Glory Kim <glorydkim@gmail.com>
2017-10-30 22:08:52 -0400

    Update Solution.java

--- Day 7 - Arrays/Solution.java
+++ Day 7 - Arrays/Solution.java
@@ -35,11 +35,15 @@ import java.util.*;
 public class Solution {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-      int n = in.nextInt();
-      int[] arr = new int[n];
-      for(int i=0; i < n; i++){
-        arr[i] = in.nextInt();
-      }
-      in.close();
+    int n = in.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+      arr[i] = in.nextInt();
+    }
+    while(n > 0){
+      System.out.print(arr[--n] + " ");
+    }
+    System.out.println("");
+    in.close();
   }
 }
