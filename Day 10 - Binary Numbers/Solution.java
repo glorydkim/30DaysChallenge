68bcfd48e496c16a2733f454a13fc0e08b5f6ed9
Glory Kim <glorydkim@gmail.com>
2017-10-31 23:28:32 -0400

    Update Solution.java

--- Day 10 - Binary Numbers/Solution.java
+++ Day 10 - Binary Numbers/Solution.java
@@ -10,5 +10,29 @@ import java.util.*;
 public class Solution {
   public static void main(String[] args) {
   /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int bin = 0, s = 0, t = 0;
+
+
+    while(n>0)
+        {
+        bin = n%2;
+        n=n/2;
+        if(bin == 1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+
+    System.out.println(t);
+    
   }
 }
