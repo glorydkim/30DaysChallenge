854c3698ec796f7c8af150534c1a0b6e930766a9
Glory Kim <glorydkim@gmail.com>
2017-10-31 23:32:41 -0400

    Update Solution.java

--- Day 11 - 2D Arrays/Solution.java
+++ Day 11 - 2D Arrays/Solution.java
@@ -9,7 +9,7 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int arr[][] = new int[6][6];
@@ -18,5 +18,28 @@ public class Solution {
                 arr[i][j] = in.nextInt();
             }
         }
+        
+        int hourglass = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for(int k = 0; k < 4; k++) {
+        	for(int l = 0; l < 4; l++) {
+        		
+        		hourglass = arr[k][l] + arr[k][l+1] 
+                + arr[k][l+2] 
+        		+ arr[k+1][l+1] 
+        		+ arr[k+2][l] 
+                + arr[k+2][l+1] 
+                + arr[k+2][l+2]; 
+        		
+        		if (hourglass > max) {
+        			max = hourglass;
+        		}
+        	}
+        }
+        
+        System.out.println(max);
+        in.close();
     }
 }
+
